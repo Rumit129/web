a0ade0cf4e9520e47acdc3bc2f1047d4e746bb53
Rumit <tima_duryagin@vk.com>
2026-04-06 18:15:46 +0300

    secondd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 
+from waitress import serve
 from flask import Flask
 
 app = Flask(__name__)
@@ -11,5 +12,4 @@ def index():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    serve(app, host='0.0.0.0', port=5000)
